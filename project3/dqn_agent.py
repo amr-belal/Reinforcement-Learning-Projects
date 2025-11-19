dd80df8e4b8821c54236773f65c2d9f33c79d8d3
amr-belal <amrbelal852@gmail.com>
2025-11-19 16:40:15 +0200

    finish setup finally and start with the agent

---
+++ project3/dqn_agent.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import random
+import collections
+import numpy as np
+
+
+# TODO: Implement DQN Agent => CNN Network, Replay Buffer, Action Selection, Training Step
+
+class Network :
+    def __init__(self , input_shape , n_actions  ):
+        pass 
